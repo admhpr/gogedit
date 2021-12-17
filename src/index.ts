9aa8e4d61e7988c17f54232e5247786b188fa5fd
adam <adam@harpur.io>
2021-12-17 15:15:33 +0000

    chore: init orm

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("hello world")
+import { MikroORM} from "@mikro-orm/core"
+
+async function main(){
+    const orm = await MikroORM.init({
+        dbName: '',
+        type: 'postgresql',
+        debug: process.env.NODE_ENV !== 'production'
+    })
+
+}
+
+
+
+main()

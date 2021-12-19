d3cdc4b08a8ab030f3e370906ed254df4bfe9f93
adam <adam@harpur.io>
2021-12-19 10:16:30 +0000

    chore: first post

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { MikroORM } from "@mikro-orm/core";
 import { IS_PRODUCTION } from "./constants";
+import { Post } from "./entities/Post";
 async function main() {
   const orm = await MikroORM.init({
     dbName: "",
     type: "postgresql",
     debug: !IS_PRODUCTION,
   });
+
+  const post = orm.em.create(Post, { title: "first post" });
 }
 
 main();

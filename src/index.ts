f1090b03c8aefab54c96eac7aadc24ec5cf64593
adam <adam@harpur.io>
2021-12-19 10:29:28 +0000

    chore: orm config

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
 import { config } from "dotenv";
 config();
 import { MikroORM } from "@mikro-orm/core";
-import { IS_PRODUCTION } from "./constants";
 import { Post } from "./entities/Post";
+import ormConfig from "./mikro-orm.config";
 async function main() {
-  const orm = await MikroORM.init({
-    dbName: "",
-    type: "postgresql",
-    debug: !IS_PRODUCTION,
-  });
+  const orm = await MikroORM.init(ormConfig);
 
   const post = orm.em.create(Post, { title: "first post" });
   await orm.em.persistAndFlush(post);
--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,7 +1,9 @@
+import { MikroORM } from "@mikro-orm/core";
+import { IS_PRODUCTION } from "./constants";
 import { Post } from "./entities/Post";
-
 export default {
   entities: [Post],
   dbName: process.env.DB_NAME,
   type: `postgresql`,
-};
+  debug: !IS_PRODUCTION,
+} as Parameters<typeof MikroORM.init>[0];

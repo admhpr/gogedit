38824607a05d612bf855cc358960b2b9177b0a02
adam <adam@harpur.io>
2021-12-21 16:50:55 +0000

    chore: setup migrations

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,10 +1,11 @@
 import { MikroORM } from "@mikro-orm/core";
 import { IS_PRODUCTION } from "./constants";
 import { Post } from "./entities/Post";
+import path from "path";
 
 export const ormConfig = {
   migrations: {
-    path: "",
+    path: path.join(__dirname, "./migrations"),
     pattern: /^[\w-]+\d+\.[tj]s$/,
   },
   entities: [Post],

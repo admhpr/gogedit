7965a94dd1bc23287c48d934a781b82b47952bf3
adam <adam@harpur.io>
2021-12-22 11:23:53 +0000

    chore: default for created

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -5,7 +5,7 @@ export class Post {
   @PrimaryKey()
   id!: number;
 
-  @Property({ type: "date" })
+  @Property({ type: "date", default: "NOW();" })
   createdAt = new Date();
 
   @Property({ type: "date", onUpdate: () => new Date() })

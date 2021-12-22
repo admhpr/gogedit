a8c890fd7d797ba67bc585a0f7962ac28e1d16d9
adam <adam@harpur.io>
2021-12-22 12:47:00 +0000

    chore: setup apollo

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,26 @@
 import { config } from "dotenv";
 config();
 import { MikroORM } from "@mikro-orm/core";
-import { Post } from "./entities/Post";
+// import { Post } from "./entities/Post"
 import { SERVER_PORT } from "./constants";
 import ormConfig from "./mikro-orm.config";
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 async function main() {
   const orm = await MikroORM.init(ormConfig);
   await orm.getMigrator().up();
   const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+  apolloServer.applyMiddleware({ app });
   app.listen(SERVER_PORT, () => {
     console.log(`app started on ${SERVER_PORT}`);
   });

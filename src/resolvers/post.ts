e2a83546c1d22747020e9026d7c477f5ed2c4504
adam <adam@harpur.io>
2021-12-22 16:35:12 +0000

    chore: create post

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,5 +1,5 @@
 import { Post } from "@entities/Post";
-import { Arg, Ctx, Int, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
 import { Context } from "../types";
 
 @Resolver()
@@ -9,10 +9,16 @@ export class PostResolver {
     return em.find(Post, {});
   }
   @Query(() => Post, { nullable: true })
-  post(
-    @Arg("id", () => Int) id: number,
-    @Ctx() { em }: Context
-  ): Promise<Post | null> {
+  post(@Arg("id") id: number, @Ctx() { em }: Context): Promise<Post | null> {
     return em.findOne(Post, { id });
+  }
+  @Mutation(() => Post)
+  async createPost(
+    @Arg("title") title: string,
+    @Ctx() { em }: Context
+  ): Promise<Post> {
+    const post = em.create(Post, { title });
+    await em.persistAndFlush(post);
+    return post;
   }
 }

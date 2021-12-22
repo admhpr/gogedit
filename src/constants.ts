0d27c6ce35720ab28fc55cfe02b5d9ea21747b6a
adam <adam@harpur.io>
2021-12-22 12:35:44 +0000

    chore: setup express

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,2 @@
 export const IS_PRODUCTION = process.env.NODE_ENV === "production";
+export const SERVER_PORT = `4000`;
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,17 @@ import { config } from "dotenv";
 config();
 import { MikroORM } from "@mikro-orm/core";
 import { Post } from "./entities/Post";
+import { SERVER_PORT } from "./constants";
 import ormConfig from "./mikro-orm.config";
+import express from "express";
 
 async function main() {
   const orm = await MikroORM.init(ormConfig);
   await orm.getMigrator().up();
-  const post = orm.em.create(Post, { title: "first post" });
-  await orm.em.persistAndFlush(post);
+  const app = express();
+  app.listen(SERVER_PORT, () => {
+    console.log(`app started on ${SERVER_PORT}`);
+  });
 }
 
 main().catch((error) => console.error(error));

1c18d1b5f18d64f7b72e37e30778e01027cb3c99
adam <adam@harpur.io>
2021-12-23 20:09:06 +0000

    chore: update app context type

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,9 @@
 import { EntityManager } from "@mikro-orm/postgresql";
 import { Request, Response } from "express";
+import { Session } from "express-session";
 
 export type AppContext = {
   em: EntityManager;
-  req: Request & { session: { userId: number } };
+  req: Request & { session: Session & { userId: number } };
   res: Response;
 };

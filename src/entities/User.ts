32e131d2559483c44c1ba2bdade6d8360fdea3c4
adam <adam@harpur.io>
2021-12-23 12:51:34 +0000

    chore: setup user entity

---
+++ src/entities/User.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, Int, ObjectType } from "type-graphql";
+
+@ObjectType()
+@Entity()
+export class User {
+  @Field(() => Int)
+  @PrimaryKey()
+  id!: number;
+
+  @Field(() => String)
+  @Property({ type: "date", default: "NOW();" })
+  createdAt = new Date();
+
+  @Field(() => String)
+  @Property({ type: "date", onUpdate: () => new Date() })
+  updatedAt = new Date();
+
+  @Field()
+  @Property({ type: "text", unique: true })
+  username!: string;
+}

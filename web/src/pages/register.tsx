e38e337d906fceca816fc20f60b695ff80088f8c
adam <adam@harpur.io>
2021-12-27 15:00:23 +0000

    chore: initial form

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
-
-interface RegisterProps {
-
-}
+import React from "react";
+import { Formik, Form } from "formik";
+import { FormControl, FormLabel, Input } from "@chakra-ui/react";
+interface RegisterProps {}
 
 const Register: React.FC<RegisterProps> = ({}) => {
-    return <div>register page</div>;
-}
+  return (
+    <Formik
+      initialValues={{ username: "", password: "" }}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      {({ values, handleChange }) => (
+        <Form>
+          <FormControl isRequired>
+            <FormLabel htmlFor="username">Username</FormLabel>
+            <Input
+              value={values.username}
+              onChange={handleChange}
+              id="username"
+              placeholder="Username"
+            />
+          </FormControl>
+          <FormControl isRequired>
+            <FormLabel htmlFor="first-name"></FormLabel>
+            <Input id="first-name" placeholder="First name" />
+          </FormControl>
+        </Form>
+      )}
+    </Formik>
+  );
+};
 
-export default Register
+export default Register;

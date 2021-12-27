eb917c1e8ae8c9f977abd86294a1eab3f06fbf5e
adam <adam@harpur.io>
2021-12-27 15:03:03 +0000

    chore: setup wrapper

---
+++ web/src/components/Wrapper.tsx
@@ -0,0 +1,8 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+interface WrapperProps {}
+
+export const Wrapper: React.FC<WrapperProps> = ({ children }) => {
+  return <Box>{children}</Box>;
+};

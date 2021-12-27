0073d532eb45ab3c4e15f85ad302ad4ceed7e966
adam <adam@harpur.io>
2021-12-27 15:07:10 +0000

    style: wrapper layout

--- web/src/components/Wrapper.tsx
+++ web/src/components/Wrapper.tsx
@@ -1,8 +1,22 @@
 import { Box } from "@chakra-ui/react";
 import React from "react";
 
-interface WrapperProps {}
+interface WrapperProps {
+  variant?: "small" | "regular";
+}
 
-export const Wrapper: React.FC<WrapperProps> = ({ children }) => {
-  return <Box>{children}</Box>;
+export const Wrapper: React.FC<WrapperProps> = ({
+  children,
+  variant = "regular",
+}) => {
+  return (
+    <Box
+      mt={8}
+      mx="auto"
+      maxW={variant === "regular" ? "800px" : "400px"}
+      w="100%"
+    >
+      {children}
+    </Box>
+  );
 };
--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -1,34 +1,37 @@
 import React from "react";
 import { Formik, Form } from "formik";
 import { FormControl, FormLabel, Input } from "@chakra-ui/react";
+import { Wrapper } from "src/components/Wrapper";
 interface RegisterProps {}
 
 const Register: React.FC<RegisterProps> = ({}) => {
   return (
-    <Formik
-      initialValues={{ username: "", password: "" }}
-      onSubmit={(values) => {
-        console.log(values);
-      }}
-    >
-      {({ values, handleChange }) => (
-        <Form>
-          <FormControl isRequired>
-            <FormLabel htmlFor="username">Username</FormLabel>
-            <Input
-              value={values.username}
-              onChange={handleChange}
-              id="username"
-              placeholder="Username"
-            />
-          </FormControl>
-          <FormControl isRequired>
-            <FormLabel htmlFor="first-name"></FormLabel>
-            <Input id="first-name" placeholder="First name" />
-          </FormControl>
-        </Form>
-      )}
-    </Formik>
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ values, handleChange }) => (
+          <Form>
+            <FormControl isRequired>
+              <FormLabel htmlFor="username">Username</FormLabel>
+              <Input
+                value={values.username}
+                onChange={handleChange}
+                id="username"
+                placeholder="Username"
+              />
+            </FormControl>
+            <FormControl isRequired>
+              <FormLabel htmlFor="first-name"></FormLabel>
+              <Input id="first-name" placeholder="First name" />
+            </FormControl>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
   );
 };
 

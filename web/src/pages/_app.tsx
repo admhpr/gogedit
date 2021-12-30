6266f97d0b31e4b7c6c3b6d4234b094cfa7ec464
adam <adam@harpur.io>
2021-12-30 09:37:59 +0000

    chore: add urql provider

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -7,15 +7,17 @@ const client = createClient({ url: `http://localhost:4000/graphql` });
 
 function App({ Component, pageProps }) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <ColorModeProvider
-        options={{
-          useSystemColorMode: true,
-        }}
-      >
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ChakraProvider>
+    <Provider value={client}>
+      <ChakraProvider resetCSS theme={theme}>
+        <ColorModeProvider
+          options={{
+            useSystemColorMode: true,
+          }}
+        >
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ChakraProvider>
+    </Provider>
   );
 }
 

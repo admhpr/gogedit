34185fb11ef86f07aa487b54ecee406f7d14daf8
adam <adam@harpur.io>
2022-01-09 10:55:03 +0000

    feat: initial login form

---
+++ web/src/pages/login.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Formik, Form } from "formik";
+import { Button } from "@chakra-ui/react";
+import { Wrapper } from "src/components/Wrapper";
+import { InputField } from "src/components/InputField";
+import { useRegisterMutation } from "src/generated/graphql";
+import { toErrorMap } from "src/utils/toErrorMap";
+import { responseSymbol } from "next/dist/server/web/spec-compliant/fetch-event";
+import { Router, useRouter } from "next/router";
+
+interface RegisterProps {}
+
+const Register: React.FC<RegisterProps> = ({}) => {
+  const router = useRouter();
+  const [, register] = useRegisterMutation();
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={async (values, { setErrors }) => {
+          const { data } = await register(values);
+          if (data?.register.errors) {
+            const { register } = data;
+            setErrors(toErrorMap(register.errors!));
+          } else if (data?.register.user) {
+            router.push("/");
+          }
+          return data;
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              placeholder="username"
+              label="Username"
+            />
+            <InputField
+              name="password"
+              placeholder="password"
+              label="Password"
+            />
+            <Button
+              mt={4}
+              type="submit"
+              isLoading={isSubmitting}
+              colorScheme="teal"
+            >
+              Register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Register;
